1db8722115bbc2494277e2c070016cf869d43758
yaroslav-bodnar <50916274+yaroslav-bodnar@users.noreply.github.com>
2019-08-17 20:30:09 +0300

    update script.js
    1.  Перетаскивание

--- src/js/script.js
+++ src/js/script.js
@@ -1465,5 +1465,91 @@ mailInput.addEventListener('invalid', function (evt) {
 console.log('=====================================ПЕРЕТАСКИВАНИЕ======================================')
 //=======================================================================ПЕРЕТАСКИВАНИЕ=====================================================
 
-var buttonHandle = document.querySelector('section_for_button');
 
+
+// var ball = document.getElementById('ball');
+// // var setup = document.querySelector('.section_for_button');
+
+// ball.addEventListener('mousedown', function(evt) {
+// 	evt.preventDefault();
+
+// 	var startCoords = {
+// 		x: evt.clientX,
+// 		y: evt.clientY
+// 	};
+
+// 	var onMouseMove = function(moveEvt) {
+// 		moveEvt.preventDefault();
+
+// 		var shift = {
+// 			x: startCoords.x - moveEvt.clientX,
+// 			y: startCoords.y - moveEvt.clientY
+// 		};
+
+// 		startCoords = {
+// 			x: moveEvt.clientX,
+// 			y: moveEvt.clientY
+// 		};
+
+// 		ball.style.top = (ball.offsetTop - shift.y) + 'px';
+// 		ball.style.left = (ball.offsetLeft - shift.x) + 'px';
+// 	};
+
+// 	var onMouseUp = function(upEvt) {
+// 		upEvt.preventDefault();
+
+// 		document.removeEventListener('mousemove', onMouseMove);
+// 		document.removeEventListener('mouseup', onMouseUp);
+// 	};
+
+// 	document.addEventListener('mousemove', onMouseMove);
+// 	document.addEventListener('mouseup', onMouseUp);
+// });
+
+
+
+
+
+
+
+// var ball = document.getElementById('ball');
+
+// ball.onmousedown = function(e) {
+
+//   var coords = getCoords(ball);
+//   var shiftX = e.pageX - coords.left;
+//   var shiftY = e.pageY - coords.top;
+
+//   ball.style.position = 'absolute';
+//   document.body.appendChild(ball);
+//   moveAt(e);
+
+//   ball.style.zIndex = 1000; // над другими элементами
+
+//   function moveAt(e) {
+//     ball.style.left = e.pageX - shiftX + 'px';
+//     ball.style.top = e.pageY - shiftY + 'px';
+//   }
+
+//   document.onmousemove = function(e) {
+//     moveAt(e);
+//   };
+
+//   ball.onmouseup = function() {
+//     document.onmousemove = null;
+//     ball.onmouseup = null;
+//   };
+
+// }
+
+// ball.ondragstart = function() {
+//   return false;
+// };
+
+// function getCoords(elem) {   // кроме IE8-
+//   var box = elem.getBoundingClientRect();
+//   return {
+//     top: box.top + pageYOffset,
+//     left: box.left + pageXOffset
+//   };
+// }
